44df482554c80e8096f7f3cecf9888bd7d8b1b13
sudopain <sudopain@gmail.com>
2021-12-15 05:16:30 +0900

    add unfollow feature

--- routes/user.js
+++ routes/user.js
@@ -61,7 +61,6 @@ router.put("/:id/follow", async (req, res) => {
       } else {
         res.status(403).json("You already follow this user");
       }
-      res.status(200).json("Account has been updated.");
     } catch (err) {
       res.status(500).json(err);
     }
@@ -70,5 +69,24 @@ router.put("/:id/follow", async (req, res) => {
   }
 });
 //unfolow
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("user has been unfollowed.");
+      } else {
+        res.status(403).json("You don't follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can't unfollow yourself.");
+  }
+});
 
 module.exports = router;
