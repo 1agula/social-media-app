a61dfbcfb29031051dcd57f05cf87158bf72ab71
sudopain <sudopain@gmail.com>
2021-12-18 02:32:29 +0900

    rightbar component

--- src/components/Rightbar/index.jsx
+++ src/components/Rightbar/index.jsx
@@ -1,5 +1,127 @@
 import "./index.scss";
 
 export default function Right() {
-  return <div className="rightbar">rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img
+            src="https://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-Gift-Box-PNG-Transparent-Image-1.png"
+            alt="birthday"
+          />
+          <span className="birthdayText">
+            <b>poopoo</b> and <b>peepee</b> have a birthday today.
+          </span>
+        </div>
+        <img
+          className="rightbarAd"
+          src="https://www.advertgallery.com/wp-content/uploads/2018/02/furniture-world-thanks-a-lot-sale-minimum-50-off-ad-times-of-india-hyderabad-09-02-2018.png"
+          alt="ad"
+        />
+        <h4 className="rightbarTitle">Online Frined</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarPfp">
+              <img
+                src="https://icon-library.com/images/aesthetic-icon/aesthetic-icon-21.jpg"
+                alt="pfp"
+              />
+              <span className="badge"></span>
+            </div>
+            <span className="rightbarName">skrr skrr</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
